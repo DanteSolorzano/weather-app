e6863d2a4c29c3782e4ee3ee4395fe6614f7c3df
DanteSolorzano <solorzanodante19@gmail.com>
2025-08-05 18:57:48 -0600

    feat: initialize CSS modules setup, create Alert and Form components, and add Data folder with country types
    - Started using CSS Modules for styling
    - Created Alert component for validation messages
    - Created Form component to render the API-connected form
    - Added Data folder containing countries and their codes as a TypeScript type

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,69 @@
+import { useState, type ChangeEvent } from "react";
+import type { SearchType } from "../../types";
+import { countries } from "../../data/countries";
+import styles from './Form.module.css'
+import Alert from "../Alert/Alert";
+
+export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const[alert, setAlert] = useState('')
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if(Object.values(search).includes('')){
+            setAlert('All the fields are required')
+            return
+        }
+    }
+
+  return (
+    <form className={styles.form}
+    onSubmit={handleSubmit}
+    >
+        {alert && <Alert>{alert}</Alert>}
+      <div className={styles.field}>
+        <label htmlFor="city">City:</label>
+        <input type="text"
+                id="city"
+                name="city"
+                placeholder="City"
+                value={search.city}
+                onChange={handleChange}
+        />
+      </div>
+      <div className={styles.field}>
+        <label htmlFor="country">Country:</label>
+        <select 
+        id="country"
+        name="country"
+        onChange={handleChange}
+        value={search.country}>
+            <option value="">--Select A Country: --</option>
+            {countries.map(country => (
+                <option
+                    key={country.code}
+                    value={country.code}
+                >{country.name}</option>
+            ))}
+        </select>
+        
+      </div>
+
+      <input className={styles.submit} type="submit"  value="Check Weather" />
+    </form>
+  )
+}
---
+++ src/data/countries.ts
@@ -0,0 +1,21 @@
+import type { Country } from "../types"
+
+export const countries : Country[] = [
+    { code: 'US', name: 'United States' },
+    { code: 'MX', name: 'Mexico' },
+    { code: 'AR', name: 'Argentina' },
+    { code: 'CO', name: 'Colombia' },
+    { code: 'CR', name: 'Costa Rica' },
+    { code: 'ES', name: 'Spain' },
+    { code: 'PE', name: 'Peru' },
+    { code: 'CA', name: 'Canada' },
+    { code: 'BR', name: 'Brazil' },
+    { code: 'CL', name: 'Chile' },
+    { code: 'FR', name: 'France' },
+    { code: 'DE', name: 'Germany' },
+    { code: 'IT', name: 'Italy' },
+    { code: 'JP', name: 'Japan' },
+    { code: 'CN', name: 'China' },
+    { code: 'AU', name: 'Australia' },
+    { code: 'IN', name: 'India' }
+  ]
