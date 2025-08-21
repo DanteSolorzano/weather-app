079f591c04fafeb8fa2c7f2e66653c1df536df24
DanteSolorzano <solorzanodante19@gmail.com>
2025-08-20 22:28:03 -0600

    feat: integrate Zod for API validation, add loading spinner, and improve error handling
    - Fetches weather data from the API and displays it by city
    - Uses Zod to validate API responses
    - Adds a loading spinner while fetching data
    - Handles errors gracefully for better UX

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import type { SearchType } from '../types'
 import { z } from 'zod'
+import { useMemo, useState } from 'react'
+//import { object, string, number, Output, parse } from 'valibot'
 
 //TYPE GARD o ASSERTION
 // function isWeatherResponse(weather : unknown) : weather is Weather{
@@ -15,23 +17,50 @@ import { z } from 'zod'
 // }
 
 //Zod
-const Weather = z.object({
-    name: z.string(),
-    main: z.object({
-        temp: z.number(),
-        temp_max: z.number(),
-        temp_min: z.number(),
-    })
+ const Weather = z.object({
+     name: z.string(),
+     main: z.object({
+         temp: z.number(),
+         temp_max: z.number(),
+         temp_min: z.number(),
+     })
 
-})
+ })
+export type Weather = z.infer<typeof Weather>
 
-type Weather = z.infer<typeof Weather>
+//Valibot
+// const WeatherSchema = object({
+//     name: string(),
+//     main: object({
+//         temp: number(),
+//         temp_max: number(),
+//         temp_min: number()
+//     })
+// })
+
+const initialState = {
+    name: '',
+    main: {
+        temp: 0,
+        temp_max: 0,
+        temp_min: 0
+    }
+}
+
 
 export default function useWeather() {
+
+    const [weather, setWeather] = useState<Weather>(initialState)
+
+    const [loading, setLoading] = useState(false)
+
+    const [notFound, setNotFound] = useState(false)
 
     const fetchWeather = async (search: SearchType) => {
 
         const appId = import.meta.env.VITE_API_KEY
+        setLoading(true)
+        setWeather(initialState)
 
         try {
 
@@ -39,6 +68,12 @@ export default function useWeather() {
 
 
             const { data } = await axios(geoUrl)
+
+            //comprobar si existe 
+            if(!data[0]){
+                setNotFound(true)
+                return
+            }
 
             const lat = data[0].lat
             const lon = data[0].lon
@@ -53,11 +88,19 @@ export default function useWeather() {
             // }
 
             //Zod
-             const { data : weatherResult } = await axios(weatherUrl)
-             const result = Weather.safeParse(weatherResult)
-             if(result.success) {
-                console.log(result.data.main.temp)
-             }
+              const { data : weatherResult } = await axios(weatherUrl)
+              const result = Weather.safeParse(weatherResult)
+              if(result.success) {
+                 setWeather(result.data)
+                 setNotFound(false)
+              }
+
+            //Valibot
+            // const { data : weatherResult } = await axios(weatherUrl) 
+            // const result = parse(WeatherSchema, weatherResult)
+            // if(result){
+            //     console.log(result.name)
+            // }
 
 
 
@@ -65,10 +108,18 @@ export default function useWeather() {
             
         } catch (e) {
             console.log(e)
+        } finally {
+            setLoading(false)
         }
     }
 
+    const hasWeatherData = useMemo(() => weather.name, [weather])
+
     return {
-        fetchWeather
+        weather,
+        loading,
+        notFound,
+        fetchWeather,
+        hasWeatherData
     }
 }
